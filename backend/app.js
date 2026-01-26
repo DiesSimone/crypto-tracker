dd8c94b09632ea753eb28eb3d2e37106405f4f92
DiesSimone <simone.dies@issgreppi.it>
2026-01-26 18:26:17 +0100

    Added fallback function in case API requests exceed the limit

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express(); //creating the app
 const cors = require("cors");
 const pricesRoute = require("./src/routes/prices_route.js");
-const portfolioRoute = require("./src/routes/portfolio_route.js");
+// const portfolioRoute = require("./src/routes/portfolio_route.js");
 
 app.use(cors());//accepts requests from any domain
 app.use(express.json()); //interprets all JSON requests
